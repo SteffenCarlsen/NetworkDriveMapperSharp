90fc6ce67564382daca7e18039f777079fe0bf18
TaF <bstaffe@gmail.com>
2022-07-26 02:17:53 +0200

    Add comments and show intent

--- NetworkDriveMapperSharp/NetworkedDrive.cs
+++ NetworkDriveMapperSharp/NetworkedDrive.cs
@@ -6,9 +6,18 @@ namespace NetworkDriveMapperSharp;
 
 public static class NetworkedDrive
 {
+    /// <summary>
+    /// Maps a network drive to the machine
+    /// </summary>
+    /// <param name="sDriveLetter">The drive letter to map the drive as. Expected format as single char</param>
+    /// <param name="sNetworkPath">Path to the network drive</param>
+    /// <param name="username">Username if required. If option is left empty, the function makes a connection to the network resource without redirecting a local device</param>
+    /// <param name="password">Password if required</param>
+    /// <param name="options">Additional options, should only be used by advanced usecases</param>
+    /// <returns></returns>
     public static int MapNetworkDrive(string sDriveLetter, string sNetworkPath, string username = "", string password = "", ConnectionOptions options = ConnectionOptions.NONE)
     {
-        //Checks if the last character is \ as this causes error on mapping a drive.
+        // Checks if the last character is \ as this causes error on mapping a drive.
         if (sNetworkPath.Substring(sNetworkPath.Length - 1, 1) == @"\")
         {
             sNetworkPath = sNetworkPath.Substring(0, sNetworkPath.Length - 1);
@@ -19,8 +28,7 @@ public static class NetworkedDrive
         oNetworkResource.sLocalName = sDriveLetter + ":";
         oNetworkResource.sRemoteName = sNetworkPath;
 
-        //If Drive is already mapped disconnect the current 
-        //mapping before adding the new mapping
+        // If Drive is already mapped disconnect the current mapping before adding the new mapping
         if (DriveHelpers.IsDriveMapped(sDriveLetter))
         {
             DisconnectNetworkDrive(sDriveLetter, true);
@@ -34,6 +42,13 @@ public static class NetworkedDrive
         return returnCode;
     }
 
+    /// <summary>
+    /// Disconnects a network drive
+    /// </summary>
+    /// <param name="sDriveLetter">Driveletter to disconnect. Expected input is a single char</param>
+    /// <param name="bForceDisconnect">Boolean value indcicating if the disconnection should happen forcefully</param>
+    /// <param name="extraFlags">Advanced extra flags. Should only be used by advanced users</param>
+    /// <returns></returns>
     public static int DisconnectNetworkDrive(string sDriveLetter, bool bForceDisconnect, ConnectionOptions extraFlags = ConnectionOptions.NONE)
     {
         return MPR.WNetCancelConnection2(sDriveLetter + ":", (uint) extraFlags, bForceDisconnect);
